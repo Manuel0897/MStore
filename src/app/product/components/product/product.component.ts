cef309831c56e07f8077eeb1747c25bf864b7889
Manuel Mendoza <juanmanuel.mendoza@globant.com>
2021-08-06 15:17:13 -0500

    product component

---
+++ src/app/product/components/product/product.component.ts
@@ -0,0 +1,36 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit
+} from '@angular/core';
+
+import { Product } from '../../../core/models/product.model';
+import { CartService } from './../../../core/services/cart.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  @Input() product!: Product;
+  @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  today = new Date();
+
+  constructor(
+    private cartService: CartService
+  ) {
+  }
+
+  ngOnInit() {
+  }
+
+  addCart() {
+    this.cartService.addCart(this.product);
+  }
+
+}
